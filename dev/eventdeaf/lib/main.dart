027517f6d4f39a981efca20ea9e9efa8e44c5aad
Kevin <kevinlory31@gmail.com>
2024-11-25 10:19:53 +0100

    fix: update for android studio

--- dev/eventdeaf/lib/main.dart
+++ dev/eventdeaf/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'EventDeaf',
+      title: 'TEST',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 0, 0)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Projet EventDeaf'),
+      home: const MyHomePage(title: 'Projet test'),
     );
   }
 }
