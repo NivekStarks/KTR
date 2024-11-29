472c869a68c141f1f143890f59390ae078b934fa
Timothée Josso <timothee-josso@hotmail.fr>
2024-11-29 16:27:56 +0100

    add file json

--- dev/eventdeaf/lib/main.dart
+++ dev/eventdeaf/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -26,42 +28,96 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final MapController _mapController = MapController();
+  List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMarkers();
+  }
+
+  Future<void> _loadMarkers() async {
+    final String response = await rootBundle.loadString('assets/markers.json');
+    final List<dynamic> data = json.decode(response);
+
+    setState(() {
+      _markers = data.map((markerData) {
+        final lat = markerData['latitude'];
+        final lng = markerData['longitude'];
+        final name = markerData['name'];
+        final category = markerData['category'];
+        final address = markerData['address'];
+        final date = markerData['date'];
+
+        return Marker(
+          point: LatLng(lat, lng),
+          width: 80,
+          height: 80,
+          child: GestureDetector(
+            onTap: () => _showPopup(context, name, category, address, date),
+            child: Column(
+              children: [
+                Icon(
+                  Icons.location_pin,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ],
+            ),
+          ),
+        );
+      }).toList();
+    });
+  }
+
+
+  void _showPopup(BuildContext context, String name, String category, String address, String date) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(name),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Catégorie: $category"),
+            Text("Adresse: $address"),
+            if (date.isNotEmpty) Text("Date: $date"),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Fermer'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Application EventDeaf',
-        style: TextStyle(fontSize: 15),
+        title: Text(
+          'Application EventDeaf',
+          style: TextStyle(fontSize: 15),
         ),
       ),
       body: FlutterMap(
         mapController: _mapController,
         options: MapOptions(
           initialCenter: LatLng(48.8566, 2.3522), // Paris
-          initialZoom: 12.0, // Initial zoom level
-          maxZoom: 18.0, // Maximum zoom level (closer view)
-          minZoom: 5.0, // Minimum zoom level (farther view)
+          initialZoom: 5.0,
+          maxZoom: 18.0,
+          minZoom: 5.0,
         ),
         children: [
           TileLayer(
             urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
             subdomains: ['a', 'b', 'c'],
           ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                point: LatLng(48.8566, 2.3522),
-                width: 80,
-                height: 80,
-                child: Icon(
-                  Icons.location_pin,
-                  color: Colors.red,
-                  size: 40,
-                ),
-              ),
-            ],
-          ),
+          MarkerLayer(markers: _markers),
         ],
       ),
       floatingActionButton: Column(
@@ -69,10 +125,9 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           FloatingActionButton(
             onPressed: () {
-              // Zoom in
               _mapController.move(
-                _mapController.camera.center, 
-                _mapController.camera.zoom + 0.5
+                _mapController.camera.center,
+                _mapController.camera.zoom + 0.5,
               );
             },
             child: Icon(Icons.zoom_in),
@@ -81,10 +136,9 @@ class _MyHomePageState extends State<MyHomePage> {
           SizedBox(height: 10),
           FloatingActionButton(
             onPressed: () {
-              // Zoom out
               _mapController.move(
-                _mapController.camera.center, 
-                _mapController.camera.zoom - 0.5
+                _mapController.camera.center,
+                _mapController.camera.zoom - 0.5,
               );
             },
             child: Icon(Icons.zoom_out),
